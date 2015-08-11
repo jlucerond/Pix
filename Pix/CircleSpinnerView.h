6ef6ffb8cf80c44d78b215192f8ee37f4bbe17f1
Joe Lucero <jlucerond@gmail.com>
2015-08-11 16:56:07 -0400

    Checkpoint 38
    Adding a like button to each cell

---
+++ Pix/CircleSpinnerView.h
@@ -0,0 +1,17 @@
+//
+//  CircleSpinnerView.h
+//  Pix
+//
+//  Created by Joe Lucero on 8/11/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSpinnerView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
